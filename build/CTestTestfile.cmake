1a98dbfc6cb1064aebe69515a00c041530f1006c
varietytas <kuptsov.maksim2901@gmail.com>
2024-04-23 11:52:26 +0300

    delete build

--- build/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/Users/User/Documents/study/HSE_files/CPP/Vector
-# Build directory: C:/Users/User/Documents/study/HSE_files/CPP/Vector/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
